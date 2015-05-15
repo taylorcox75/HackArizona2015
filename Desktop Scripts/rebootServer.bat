b8164f29dd57d35ab3a1bc53fdc790b4337739fa
Taylor Cox <taylorcox75@gmail.com>
2015-05-15 06:07:04 -0700

    Initial Push.

---
+++ Desktop Scripts/rebootServer.bat
@@ -0,0 +1,11 @@
+@echo off
+break off
+color 0a
+
+cd/
+
+cd wamp/www/stream/example/createServer/
+node app.js
+
+
+cmd /K
---
+++ Desktop Scripts/serverLaunch.bat
@@ -0,0 +1,15 @@
+@echo off
+break off
+color 0a
+
+ping 192.168.43.7
+
+
+@ echo
+cd/
+
+cd wamp/www/stream/example/createServer/
+node app.js
+
+
+cmd /K
